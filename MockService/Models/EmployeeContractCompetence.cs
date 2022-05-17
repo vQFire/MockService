09d6859ded7bd85f142e078a540ada8d46905401
TijsGroenendaal <tijs.groenendaal@gmail.com>
2022-05-17 23:18:17 +0200

    change contractcompetence  to datetime

--- MockService/Models/EmployeeContractCompetence.cs
+++ MockService/Models/EmployeeContractCompetence.cs
@@ -5,6 +5,6 @@ public class EmployeeContractCompetence
     public Guid Id { get; set; }
     public EmployeeContract EmployeeContract { get; set; }
     public Competence Competence { get; set; }
-    public DateOnly validFrom { get; set; }
-    public DateOnly validTo { get; set; }
+    public DateTime validFrom { get; set; }
+    public DateTime validTo { get; set; }
 }
