2e7726be558027c6ceab49f66d1761d757cb393b
InsectByte <digitalgamernl@gmail.com>
2022-06-03 12:33:29 +0200

    Fixed some nullable issues

--- MockService/Models/ScheduleGroup.cs
+++ MockService/Models/ScheduleGroup.cs
@@ -7,7 +7,7 @@ public class ScheduleGroup
     public Guid Id { get; set; }
     public IEnumerable<OrganizationalUnitScheduleGroup> OrganizationalUnits { get; set; }
     public IEnumerable<CompetenceScheduleGroup> CompetenceScheduleGroups { get; set; }
-    public string Description { get; set; }
+    public string Description { get; set; } = string.Empty;
     public bool IsValid { get; set; }
     public bool IgNorInCalculations { get; set; }
 }
